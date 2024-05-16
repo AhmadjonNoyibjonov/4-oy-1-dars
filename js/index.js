ecf523e086cf3c92d83e98d8a1e776d4da8684e3
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-05-17 00:11:40 +0500

    project finished

---
+++ js/index.js
@@ -0,0 +1,96 @@
+// // let x = '10';
+// // let y = '5';
+// // let result = x/y
+// // let isconsole = confirm("resultni ekranga chiqarasizmi");
+// document.write(result)
+
+// 1 - masala;
+// let = A + prompt("Uzunlik sm da kiriting");
+// let metr = Math.floor [A-(A % 100)]/100;
+// console.log(metr);
+
+// 2 - masala;
+// let = T + prompt("Massani kgda kiriting");
+// let tonna = Math.floor [T-(T % 1000)]/1000;
+// console.log(tonna);
+
+// 3 - masala;
+// let = M + prompt("baytlarda kiriting");
+// let kbayt = Math.floor [M-(M % 1024)]/1024;
+// console.log(kbayt);
+
+// 4 - masala;
+// alert("a>b");
+// let a = +prompt("a kesma");
+// let b = +prompt("b kesma");
+// let c = Math.floor [a-(a % b)]/b;
+// console.log(c);
+
+// 5 - masala;
+// alert("A>B");
+// let A = +prompt("a kesma");
+// let B = +prompt("b kesma");
+// let C = Math.floor(A % B);
+// console.log(C);
+
+// 6 - masala;
+
+// let ab = +prompt("ikki xonali son kiriting");
+// let w = Math.floor [ab-(ab % 10)]/10;
+// let clutch = Math.floor(ab % 10);
+// console.log(w);
+// console.log(clutch);
+
+// 7 - masala;
+
+// let n = +prompt("n ikki xonali son kiriting");
+// let s = Math.floor [n-(n % 10)]/10;
+// let f = Math.floor(n % 10);
+// let won = Math.floor(s + f);
+// console.log(won);
+
+// 8 - masala;
+
+// let p = +prompt("p ikki xonali son kiriting");
+// let o_n = Math.floor [p-(p % 10)]/10;
+// let bir = Math.floor(p % 10);
+// let P = Math.floor(bir * 10 + o_n);
+// console.log(P);
+
+// 9 - masala;
+
+// let abc = +prompt("abc uch xonali son kiriting");
+// let ch = Math.floor [abc-(abc % 100)]/100;
+// console.log(ch);
+
+// 10 - masala;
+
+// let abd = +prompt("abd uch xonali son kitiring");
+// let bir_ = Math.floor(abd % 10);
+// let _on = Math.floor [(abd%100)-((abd%100)%10)]/10
+// console.log(bir_);
+// console.log(_on);
+
+// 11 - masala;
+
+// let www = +prompt("uch xonali son kiriting");
+// let w1 = Math.floor(www % 10);
+// let w2 = Math.floor([www-(www % 100)] % 100);
+// let w3 = Math.floor[(www%100)-((www%100)%10)]/10;
+// console.log(w1 + w2 + w3);
+
+// 12 - masala;
+
+// let aaa = prompt("uch xonali son kiriting");
+// let b1 = Math.floor( aaa % 10);
+// let y1 = Math.floor ([aaa-(aaa % 100)] % 100);
+// let o1 = Math.floor [(aaa%100)-((aaa%100)%10)]/10;
+// console.log(b1*100 + o1*10 + y1);
+
+// 13 - masala;
+
+// let AAA = +prompt("AAA uch xonali son kiriting");
+// let b21 = Math.floor ( AAA % 10);
+// let y21 = Math.floor ([AAA-(AAA % 100)] % 100);
+// let o21 = Math.floor [(AAA%100)-((AAA%100)%10)]/10;
+// console.log(y21*100 + b21*10 + o21);
